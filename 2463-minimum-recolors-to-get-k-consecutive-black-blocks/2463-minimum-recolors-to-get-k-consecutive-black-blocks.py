c3f997dc92a72993924cc0415c790d3c08c4b7dc
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2025-03-08 07:54:49 +0100

    Time: 0 ms (100%), Space: 17.7 MB (62.29%) - LeetHub

---
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/2463-minimum-recolors-to-get-k-consecutive-black-blocks.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumRecolors(self, blocks: str, k: int) -> int:
+        min_operations = float('inf')
+        current_white_count = 0
+
+        
+        for i in range(k):
+            if blocks[i] == 'W':
+                current_white_count += 1
+        
+        min_operations = current_white_count  
+
+        
+        for i in range(k, len(blocks)):
+            if blocks[i - k] == 'W': 
+                current_white_count -= 1
+            if blocks[i] == 'W':  
+                current_white_count += 1
+            
+            min_operations = min(min_operations, current_white_count)
+
+        return min_operations
