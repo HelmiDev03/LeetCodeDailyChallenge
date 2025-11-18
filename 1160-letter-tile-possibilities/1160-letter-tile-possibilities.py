22d2f46b4fee816a64458c7a942ba7bd73815b69
Helmi Lakhder <102436033+HelmiDev03@users.noreply.github.com>
2025-11-18 23:49:13 +0100

    Time: 79 ms (17.45%), Space: 19.5 MB (32.89%) - LeetHub

--- 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
+++ 1160-letter-tile-possibilities/1160-letter-tile-possibilities.py
@@ -1,14 +1,29 @@
 class Solution:
     def numTilePossibilities(self, tiles: str) -> int:
-        count = Counter(tiles)
-        res = 0
-        def dfs():
-            nonlocal res
-            for key in count:
-                if count[key]:
-                    res += 1
-                    count[key] -= 1
-                    dfs()
-                    count[key] += 1
-        dfs()
-        return res
+        def dfs (comb , i, ans,visitedasStart) :
+            while i < len (tiles):
+                if  not comb and tiles[i] in visitedasStart:
+                    i+=1
+                    continue
+                if not visited[i]:
+                    visited[i]=1
+                    if not comb:
+                        visitedasStart.append(tiles[i])
+                    comb+=tiles[i]
+                    
+                    ans.add(comb)
+                    dfs(comb,0,ans,visitedasStart)
+                    comb=comb[0:len(comb)-1]
+                    visited[i]=0   
+
+                i+=1           
+            
+
+        comb=""
+        ans=set()
+        visited=[0]*len(tiles)
+        visitedasStart=[]
+        dfs(comb,0,ans,visitedasStart)
+        return len(ans)
+        
+    
